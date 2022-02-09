8b77329195d9bd71c9567bc8fdddff334cc331d1
tawanrio <stawanrio@gmail.com>
2022-02-08 23:41:56 -0300

    fim_dia 08/12

--- public/index.php
+++ public/index.php
@@ -4,5 +4,7 @@ require_once('../src/config/config.php');
 require_once('../src/models/User.php');
 
 $user = new User(['name' => 'tawan','email' => 'stawanrio@gmail.com']);
-print_r($user->get());
-echo 'fim';
+
+echo $user->getSelect(['id' => 1]);
+echo '<br> <br>';
+echo $user->getSelect(['name' => 'chaves', 'email' => 'chaves@cod3r.com.br']);
--- src/models/Model.php
+++ src/models/Model.php
@@ -1,7 +1,7 @@
 <?php
 
     class Model{
-        protected static $tableName = '';
+        protected static $tableName = 'users';
         protected static $columns = [];
         protected $values = [];
         function __construct($arr){
@@ -10,14 +10,55 @@
         public function loadFromArray($arr){
             if($arr){
                 foreach($arr as $key => $values){
-                     $this->set($key, $values);
+                     $this->$key = $values;
                 }
             }
         }
-        public function get($key){
+        public function __get($key){
             return $this->values[$key];
         }
-        public function set($key, $value){
+        public function __set($key, $value){
             $this->values[$key] = $value;
         }
-    }
+
+       
+        
+        public static function getResultSetFromSelect($filters = [], $columns = '*'){
+            $sql = "SELECT ${columns} FROM "
+                . static::$tableName
+                . static::getFilters($filters);
+            $result = Database::getResultFromQuery($sql);
+            if($result->num_rows === 0){
+                return null;
+            }else{
+                return $result;
+            }
+        }
+        private static function getFilters($filters){
+            $sql = '';
+            if(count($filters) > 0){
+                $sql .= " WHERE 1 = 1";
+                foreach($filters as $column => $value) {
+                    $sql .= " AND ${column} = ". static::getFormatedValue($value);
+                }
+                $sql .= ';';
+            }
+            return $sql;
+        }
+        private static function getFormatedValue($value){
+            switch ($value){
+                case is_null($value):
+                    return "null";
+                    break;
+                 
+                case is_string($value):
+                    return "'$value'";
+                    break;
+                 
+                default:
+                    return $value;
+                    break;
+                 
+            }
+        }
+    } 
